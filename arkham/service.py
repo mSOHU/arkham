8f3cc99026f5eff830a12e5f4532ed4a3c3a1835
boyxuper <boyxuper@gmail.com>
2015-12-20 23:06:02 +0800

    - tweak log level for Opening connection

--- arkham/service.py
+++ arkham/service.py
@@ -122,7 +122,7 @@ class ArkhamService(object):
     @contextlib.contextmanager
     def ensure_service(self):
         if not self.connection or self.connection.is_closed:
-            LOGGER.warning('ensure_service: Opening connection...')
+            LOGGER.info('ensure_service: Opening connection...')
             self.connection = self.make_connection(self.conf)
             self.channel = self.connection.channel()
             self.invoke_connect_callback()
