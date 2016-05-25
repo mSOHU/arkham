0e11078a722607e8210b76c004a9075cced66d22
boyxuper <boyxuper@gmail.com>
2016-05-25 11:30:02 +0800

    upgrade to gevent 1.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         'pyyaml',
         'pika==0.10.1-dev1',
-        'gevent==1.1rc1',
+        'gevent==1.1.0',
     ],
     entry_points={
         'console_scripts': [
